84b87940633da3b8c25161c93abcb80bf189ba80
Prajwal jotiram Sankpal <prajwalsankpal7896@gmail.com>
2026-05-11 23:36:25 +0530

    Update profit_analysis.py

--- profit_analysis.py
+++ profit_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("../data/farmers_market_master.csv")
+data = pd.read_csv("farmers_market_master.csv")
 data['date'] = pd.to_datetime(data['date'])
 
 data['profit'] = (data['price_per_kg'] - data['cost_per_kg']) * data['quantity_sold_kg']
